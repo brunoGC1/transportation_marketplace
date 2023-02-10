fa36db6c2750d0a964aa80302ec8c03ebadf832c
Milena Maeda <milenaminekawa@gmail.com>
2023-02-10 18:34:06 -0300

    Added table and fixed the calendar

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -1,11 +1,28 @@
 import { Controller } from "@hotwired/stimulus"
 import flatpickr from "flatpickr";
 
+
 // Connects to data-controller="flatpickr"
 export default class extends Controller {
   static targets = [ "startTime", "endTime" ]
   connect() {
-    flatpickr(this.startTimeTarget, {})
-    flatpickr(this.endTimeTarget, {})
+    // flatpickr(this.startTimeTarget, {})
+    // flatpickr(this.endTimeTarget, {})
+    flatpickr(this.startTimeTarget, {
+      minDate: "today",
+      dateFormat: "d.m.Y",
+      altInput: true,
+    });
+    flatpickr(this.endTimeTarget, {
+      dateFormat: "d.m.Y",
+      altInput: true,
+    });
   }
 }
+
+// "disable": [
+//   function(date) {
+//       // return true to disable
+//       return (date.getDay() === 0 || date.getDay() === 6);
+
+//   }
